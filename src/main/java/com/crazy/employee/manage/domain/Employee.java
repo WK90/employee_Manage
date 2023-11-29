ef630ed208e0932a9fd932a52b2e18613d9f9383
zhxm <17860552235@163.com>
2023-11-29 23:07:01 +0800

    增加@Data

--- src/main/java/com/crazy/employee/manage/domain/Employee.java
+++ src/main/java/com/crazy/employee/manage/domain/Employee.java
@@ -2,6 +2,7 @@ package com.crazy.employee.manage.domain;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 
 import java.io.Serializable;
 
@@ -13,6 +14,7 @@ import java.io.Serializable;
  * @date 2023-11-29
  */
 @ApiModel(value = "职工信息",description = "")
+@Data
 public class Employee implements Serializable,Cloneable{
     /** 职工ID */
     @ApiModelProperty(name = "职工ID",notes = "")
@@ -33,52 +35,52 @@ public class Employee implements Serializable,Cloneable{
     @ApiModelProperty(name = "学历",notes = "1：大专，2：本科，3：研究生")
     private Integer empDegreeName ;
 
-    /** 职工ID */
+   /* *//** 职工ID *//*
     public Integer getId(){
         return this.id;
     }
-    /** 职工ID */
+    *//** 职工ID *//*
     public void setId(Integer id){
         this.id=id;
     }
-    /** 职工姓名 */
+    *//** 职工姓名 *//*
     public String getEmpName(){
         return this.empName;
     }
-    /** 职工姓名 */
+    *//** 职工姓名 *//*
     public void setEmpName(String empName){
         this.empName=empName;
     }
-    /** 性别，1：男，2：女 */
+    *//** 性别，1：男，2：女 *//*
     public Integer getSex(){
         return this.sex;
     }
-    /** 性别，1：男，2：女 */
+    *//** 性别，1：男，2：女 *//*
     public void setSex(Integer sex){
         this.sex=sex;
     }
-    /** 年龄 */
+    *//** 年龄 *//*
     public Integer getAge(){
         return this.age;
     }
-    /** 年龄 */
+    *//** 年龄 *//*
     public void setAge(Integer age){
         this.age=age;
     }
-    /** 部门，1：业务部，2：后勤部，3：人事部 */
+    *//** 部门，1：业务部，2：后勤部，3：人事部 *//*
     public Integer getDeptName(){
         return this.deptName;
     }
-    /** 部门，1：业务部，2：后勤部，3：人事部 */
+    *//** 部门，1：业务部，2：后勤部，3：人事部 *//*
     public void setDeptName(Integer deptName){
         this.deptName=deptName;
     }
-    /** 学历，1：大专，2：本科，3：研究生 */
+    *//** 学历，1：大专，2：本科，3：研究生 *//*
     public Integer getEmpDegreeName(){
         return this.empDegreeName;
     }
-    /** 学历，1：大专，2：本科，3：研究生 */
+    *//** 学历，1：大专，2：本科，3：研究生 *//*
     public void setEmpDegreeName(Integer empDegreeName){
         this.empDegreeName=empDegreeName;
-    }
+    }*/
 }

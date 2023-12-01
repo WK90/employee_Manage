b0833a6a9d31001d7072b511f41c51204d71b701
zhxm <17860552235@163.com>
2023-12-01 09:26:47 +0800

    123

--- src/main/java/com/crazy/employee/manage/controller/EmployeeController.java
+++ src/main/java/com/crazy/employee/manage/controller/EmployeeController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,7 +28,7 @@ import java.util.Map;
 @RequestMapping("/employee")
 public class EmployeeController{
 
-    @Autowired
+    @Resource
     private EmployeeService employeeService;
 
     /**
